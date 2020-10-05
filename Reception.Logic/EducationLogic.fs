4d2f5c3f99b2a8d0aae471de66206595315df9e9
KloderGit <idzhyan.develop@outlook.com>
2020-10-05 19:04:29 +0300

    upd

--- Reception.Logic/EducationLogic.fs
+++ Reception.Logic/EducationLogic.fs
@@ -13,20 +13,42 @@ module Education =
 
     let GetAttestation = async {
             return! client.Program
+                       .IncludeDiscipline(true)
                        .Filter(fun x->x.DeletionMark = false).And()
                        .Filter(fun x ->x.Status = "Активный").And()
-                       //.Filter(fun x->x.Disciplines.Any(fun y->y.ControlTypeKey = new Guid("f4190a1a-4cdc-11e6-a716-c8600054f636")))
-                       .Filter(fun x->x.Disciplines.Any(fun y->y.ControlTypeKey <> Guid.Empty))
+                       .Filter(fun x->x.Disciplines.Any(fun y->y.ControlTypeKey = new Guid("dc91a256-8ed6-11e6-8102-10c37b94684b")))
+                       //.Filter(fun x->x.Disciplines.Any(fun y->y.ControlTypeKey <> Guid.Empty))
                        .GetByFilter() |> Async.AwaitTask }
 
+    let getDiscipline (inf:Model.DisciplineInfo) =
+        {|
+            Key = inf.DisciplineKey
+            Title = inf.Discipline.Description
+            ControlKey = inf.ControlTypeKey
+        |}
+
     let CollectDisciplines (program: Model.Program) =
-        let arr = program.Disciplines        
-        Seq.map (fun (x:Model.Discipline) -> {| Program = program.Title; DisciplineKey = x.DisciplineKey|}) arr                   
+        let arr = program.Disciplines
+        Seq.map (fun (x:Model.DisciplineInfo) -> {| Program = program.Title; Discipline = {| Key = x.DisciplineKey; Title = x.Discipline.Description  |}; ControlType = x.ControlTypeKey |}) arr
+
+    //let asd (prog:Model.Program) =
+    //    let disciplines = Seq.map getDiscipline prog.Disciplines
+    //    let coll = 
+    //        Seq.map (fun (x)->{| Key =x.Key; Title = x.Title; ControlKey =x.ControlTypeKey; Program = prog.Title |} ) disciplines
+    //    coll
 
     let Get = 
         let accumuleted = GetAttestation |> Async.RunSynchronously |> Seq.map CollectDisciplines 
         let collected = Seq.concat accumuleted
-        let grouped = collected.GroupBy(fun x->x.DisciplineKey)
-        let result = grouped |> Seq.map (fun x-> {| Discipline = x.Key; Programs = x.Select(fun y->y.Program) |}) 
+        let grouped = collected.GroupBy(fun x->x.Discipline.Key)
+        let result = 
+            grouped |> 
+            Seq.map (fun x-> 
+                {| 
+                    Key = x.Key
+                    Title = x.First().Discipline.Title
+                    ControlType = x.First().ControlType
+                    Programs = x.Select(fun y->y.Program) 
+                 |}) 
         result
         
